a5d50666f3526b93566034a7dc50b3663927539b
briannaemorales <moralesebrianna@gmail.com>
2021-05-04 02:45:59 -0700

    add auto-role

--- ewb.py
+++ ewb.py
@@ -1,11 +1,15 @@
 import discord
 import os
 from discord.ext import commands
+from discord.utils import get
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+#intents = discord.Intents.default()
+#intents.members = True
 
 bot = commands.Bot(command_prefix='^')
 
@@ -15,16 +19,24 @@ async def on_ready():
     print("ewb will save the world")
 
 
+@bot.event
+async def on_member_join(member):
+    role = get(member.guild.roles, name='EWB Members')
+    await member.add_roles(role)
+
+
 @bot.command()
 async def hi(ctx):
     await ctx.channel.send("henlo")
 
+
 @bot.command()
 async def ping(reallylongvariablename):
-	await reallylongvariablename.channel.send("pong")
+    await reallylongvariablename.channel.send("pong")
+
 
 @bot.command()
 async def bri(rtx):
-	await rtx.channel.send(rtx.author.mention + " frick")
+    await rtx.channel.send(rtx.author.mention + " frick")
 
 bot.run(TOKEN)
