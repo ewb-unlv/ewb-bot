7bfda382c3f685a3efda063009770a4c5f266a23
briannaemorales <moralesebrianna@gmail.com>
2021-05-04 01:35:31 -0700

    update command prefix

--- ewb.py
+++ ewb.py
@@ -1,2 +1,22 @@
 import discord
 import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='^')
+
+
+@bot.event
+async def on_ready():
+    print("ewb will save the world")
+
+
+@bot.command()
+async def hi(ctx):
+    await ctx.channel.send("henlo")
+
+bot.run(TOKEN)
